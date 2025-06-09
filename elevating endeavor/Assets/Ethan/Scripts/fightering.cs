32a366736962252256b7816f4250168e99876f87
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2025-06-09 13:25:26 -0700

    ok so inputs yaay

--- elevating endeavor/Assets/Ethan/Scripts/fightering.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
-
-public class fightering : MonoBehaviour
-{
-    public InputActionReference light, medium, heavy, special, block;
-
-
-}
--- elevating endeavor/Assets/Ethan/Scripts/topDownMovement.cs
+++ elevating endeavor/Assets/Ethan/Scripts/topDownMovement.cs
@@ -1,20 +1,82 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class topDownMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody rb;
     private Vector3 moveInput;
+    public float jumpForce;
     private float activeMoveSpeed;
+    public bool canJump;
     //public Animator animator;
 
+    public InputActionReference lite, medium, heavy, special, block, jump;
+
+    private void OnEnable()
+    {
+        //single click/press events
+        lite.action.performed += light_Pressed;
+        medium.action.performed += medium_Pressed;
+        heavy.action.performed += heavy_Pressed;
+        special.action.performed += special_Pressed;
+        jump.action.performed += jump_Pressed;
+        //held press events
+        block.action.performed += block_Started;
+        block.action.performed -= block_Released;
+
+        block.action.canceled += block_Released;
+        block.action.canceled -= block_Started;
+
+    }
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("floor"))
+        {
+            canJump = true;
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        canJump = false;
+    }
     // Start is called before the first frame update
     void Start()
     {
         activeMoveSpeed = moveSpeed;
     }
-
+    private void light_Pressed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Light Attack");
+    }
+    private void medium_Pressed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Medium Attack");
+    }
+    private void heavy_Pressed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Heavy Attack");
+    }
+    private void special_Pressed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Special Attack");
+    }
+    private void block_Started(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Blocked");
+    }
+    public void block_Released(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Stopped Block");
+    }
+    private void jump_Pressed(InputAction.CallbackContext obj)
+    {
+        if (canJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Acceleration);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
